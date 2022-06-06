f6d94f77f5203af243641d4f4bd6bb752e09f631
Gabriel Rzecznik <rzecznike@gmail.com>
2022-06-06 01:15:42 -0300

    map

--- controller/js/inicioBusqueda.js
+++ controller/js/inicioBusqueda.js
@@ -5,19 +5,18 @@ if (navigator.geolocation) {
         //console.log(geoLocalizacion);
         var latitud = geoLocalizacion.coords.latitude;
         var longitud = geoLocalizacion.coords.longitude;
-        console.log(latitud);
-        console.log(longitud);
+        
+        var coord = {lat:latitud ,lng: longitud};//
+        var map = new google.maps.Map(document.getElementById('map'),{
+        zoom: 15,
+        center: coord
+        });
+        var marker = new google.maps.Marker({
+        position: coord,
+        map: map
+        });
     }
 }else{
     alert("No se pudo obtener su ubicación");
 }
 
-var coord = {latitud ,longitud};//
-var map = new google.maps.Map(document.getElementById('map'),{
-zoom: 10,
-center: coord
-});
-var marker = new google.maps.Marker({
-position: coord,
-map: map
-});
