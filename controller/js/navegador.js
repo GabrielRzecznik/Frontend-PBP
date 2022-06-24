5a6d74e7de453332379c5b61a2d857aa2b3bd625
Gabriel Rzecznik <rzecznike@gmail.com>
2022-06-23 23:09:47 -0300

    test

--- controller/js/navegador.js
+++ controller/js/navegador.js
@@ -1,9 +1,10 @@
-//Mejor vulnerable
-if (!localStorage["nombreUsuario"]) {
-    window.location.href = "https://frontend-pbp.herokuapp.com/";
+window.addEventListener('load',load);
+function load(){
+    if (localStorage["nombreUsuario"]) {
+        window.location.href = "https://frontend-pbp.herokuapp.com/";
+    }
 }
-
-
+//document.getElementById('session').classList.add(localStorage);
 function cerrarSesion() {
     localStorage.clear();
     sessionStorage.clear();
