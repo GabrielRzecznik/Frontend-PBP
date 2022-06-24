20829532ac91d8c398bf72bc1f7947519b91aa58
Gabriel Rzecznik <rzecznike@gmail.com>
2022-06-23 22:14:49 -0300

    cerrar sesión

--- controller/js/navegador.js
+++ controller/js/navegador.js
@@ -2,7 +2,7 @@
 function cerrarSesion() {
     localStorage.clear();
     sessionStorage.clear();
-    window.location.href = "https://frontend-pbp.herokuapp.com/view/";
+    window.location.href = "https://frontend-pbp.herokuapp.com/";
 }
 
 document.getElementById('nombreUsuario').innerHTML = localStorage["nombreUsuario"];
