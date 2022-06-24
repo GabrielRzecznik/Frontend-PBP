78272e335c16dbfb78aaaf79fce1d09d1bb46eec
Gabriel Rzecznik <rzecznike@gmail.com>
2022-06-24 09:47:19 -0300

    error

--- controller/js/navegador.js
+++ controller/js/navegador.js
@@ -1,9 +1,4 @@
-window.addEventListener('load',load);
-function load(){
-    if (localStorage["nombreUsuario"]) {
-        window.location.href = "https://frontend-pbp.herokuapp.com/";
-    }
-}
+
 //document.getElementById('session').classList.add(localStorage);
 function cerrarSesion() {
     localStorage.clear();
