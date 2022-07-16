a37f3d70212b1c65fd7defd9a46a7015d8bb064c
Gabriel Rzecznik <rzecznike@gmail.com>
2022-07-16 03:02:35 -0300

    hora

--- controller/js/calendar.js
+++ controller/js/calendar.js
@@ -128,9 +128,10 @@ document.addEventListener("DOMContentLoaded", function () {
         
         var fecha = año + "-" + mes + "-" + dia;
 
-        var hora = getHours();
-        var minutos = getMinutes();
-        var segundos = getSeconds();
+        var h = new Date();
+        var hora = h.getHours();
+        var minutos = h.getMinutes();
+        var segundos = h.getSeconds();
         var horaActual = hora + ":" + minutos + ":" + segundos;
 
         var horaDesde = infoDesde.substring(16,24);
