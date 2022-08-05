9df98b35d2840996ecb7ac50ee28e6fa3d0375b8
Gabriel Rzecznik <rzecznike@gmail.com>
2022-08-05 07:46:43 -0300

    gestorMostrarGrilla

--- controller/js/gestorMostrarGrilla.js
+++ controller/js/gestorMostrarGrilla.js
@@ -1,10 +1,16 @@
-
-
+tipoVista = "paciente";
 
 if (tipoVista == "paciente") {
+    //Enviar AJAX según tipoVista = Paciente
+    console.log(localStorage.getItem("id_paciente"));
+    mostrarSolicitudes(id_paciente);
+    //mostrarTurnos(id_paciente);
+}else if (tipoVista == "paciente_a_profesional") {
+    //Enviar AJAX según tipoVista = Paciente a Profesional
     
-}else if (tipoVista == "profesional") {
+}else if (tipoVista == "profesional"){
+    //Enviar AJAX según tipoVista = Profesional
     
-}else if (tipoVista == "paciente_a_profesional"){
-    
+}else{
+    alert("Error inesperado: No se definieron los parametros para la generación de la grilla");
 }
---
+++ model/js/ajaxMostrarTurnos.js
@@ -0,0 +1,31 @@
+function buscarUsuario(formulario){
+    var formData = new FormData(formulario);
+    var formJSON = JSON.stringify(Object.fromEntries(formData));
+    //console.log(formJSON);
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {//Cuando hay cambio de estado disparo la function
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {//Volvio respuesta
+            if (xmlhttp.status == 200) {//Volvio Bien
+                //localStorage.setItem("correo", document.getElementById("correo").value);//Me guarda el correo
+                
+                //Tranforma la información que ataja en un JSON que puede ser leido mediante el data[][""]
+                var data=JSON.parse(xmlhttp.responseText);
+
+                localStorage.setItem("id_usuario", data[0]["id_usuario"]);//Me guarda el id_usuario de la bd
+                localStorage.setItem("id_paciente", data[0]["id_paciente"]);//Me guarda el id_usuario de la bd
+                localStorage.setItem("id_profesional", data[0]["id_profesional"]);//Me guarda el id_usuario de la bd
+                localStorage.setItem("nombreUsuario", data[0]["nombreUsuario"]);//Me guarda el nombreUsuario de la bd
+                localStorage.setItem("correo", data[0]["correo"]);//Me guarda el correo de la bd
+                
+                //Me redirige a la pagina principal
+                window.location.href = "https://frontend-pbp.herokuapp.com/view/inicioBusqueda.html";
+            }if (xmlhttp.status == 401) {
+                alert("No se encontro el usuario!");
+                //document.querySelector('#loguearse').classList.remove('invisible');
+                //document.querySelector('#cargando').classList.add('invisible');
+            }
+        }
+    }
+    xmlhttp.open("POST",'https://backend-pbp.herokuapp.com/Usuarios/buscarUsuario',true);
+    xmlhttp.send(formJSON);
+}
