c76601f046086620c4526730595ad0b617fcc101
Gabriel Rzecznik <rzecznike@gmail.com>
2022-08-10 17:30:18 -0300

    Estados

--- controller/js/seleccionRol.js
+++ controller/js/seleccionRol.js
@@ -1,11 +1,12 @@
 //Click en solo paciente
 document.getElementById("paciente").addEventListener("click", function( event ) {
-    event = console.log("Soy paciente");
-
+    //event = console.log("Soy paciente");
+    respuesta = "Activo";
+    seleccionRol(respuesta);
 });
 
 //Click en profesional
 document.getElementById("profesional").addEventListener("click", function( event ) {
-    event = console.log("Soy profesional");
-    
+    //event = console.log("Soy profesional");
+    window.location.href = "https://frontend-pbp.herokuapp.com/view/registroProfesional.html";
 });
--- model/js/ajaxSeleccionRol.js
+++ model/js/ajaxSeleccionRol.js
@@ -1,24 +1,18 @@
 function seleccionRol(respuesta){
-    var formData= new FormData(); 
+    var formData= new FormData();
+    formData.append("id_usuario", localStorage.getItem("id_usuario"), "estadoPerfil", respuesta);
+    var formJSON=JSON.stringify(Object.fromEntries(formData));
 
-    if (respuesta == "Paciente") {
-        formData.append("id_usuario", localStorage.getItem("id_usuario"), "estadoPerfil", respuesta);
-        var formJSON=JSON.stringify(Object.fromEntries(formData));
-
-        xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function () {//Cuando hay cambio de estado disparo la function
-            if (xmlhttp.readyState == XMLHttpRequest.DONE) {//Volvio respuesta
-                if (xmlhttp.status == 200) {//Volvio Bien
-                    window.location.href = "https://frontend-pbp.herokuapp.com/";
-                }else{
-                    alert("Error Seleccion de Rol!");
-                }   
-            }
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {//Cuando hay cambio de estado disparo la function
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {//Volvio respuesta
+            if (xmlhttp.status == 200) {//Volvio Bien
+                window.location.href = "https://frontend-pbp.herokuapp.com/";
+            }else{
+                alert("Error Seleccion de Rol!");
+            }   
         }
-        xmlhttp.open("PUT",'https://parcial-edi-backend.herokuapp.com/Pacientes/estadoPaciente',true);
-        xmlhttp.send(formJSON);
-
-    }else if(respuesta == "Profesional") {
-        window.location.href = "https://frontend-pbp.herokuapp.com/view/registroProfesional.html";
     }
+    xmlhttp.open("PUT",'https://parcial-edi-backend.herokuapp.com/Pacientes/estadoPaciente',true);
+    xmlhttp.send(formJSON);
 }
