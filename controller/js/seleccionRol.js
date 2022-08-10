76ebf703efaef9d43fb9d096d0b7f46047d63706
Gabriel Rzecznik <rzecznike@gmail.com>
2022-08-10 17:04:28 -0300

    Estado Pacientes

---
+++ controller/js/seleccionRol.js
@@ -0,0 +1,11 @@
+//Click en solo paciente
+document.getElementById("paciente").addEventListener("click", function( event ) {
+    event = console.log("Soy paciente");
+
+});
+
+//Click en profesional
+document.getElementById("profesional").addEventListener("click", function( event ) {
+    event = console.log("Soy profesional");
+    
+});
