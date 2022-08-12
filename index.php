f412c7415addac7a2626ac68a0d87b26d3bd1b25
Gabriel Rzecznik <rzecznike@gmail.com>
2022-08-11 21:06:08 -0300

    Test correo

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
-<link rel="icon" href="./view/img/icono.png">
+<html>
+  <head>
+  </head>
+  <body>
+    <form method="post">
+    <imput type="text" placeholder="name" name="name">
+    <imput type="email" placeholder="email" name="email">
+    <imput type="text" placeholder="asunto" name="asunto">
+      <textarea placeholder="mensaje" name="msg"></textarea>
+      <imput type="submit" name="enviar">
+</form>
 <?php
-    include_once('./view/login.html');  
+include("correo.php");
 ?>
+  </body>
+</html>
---
+++ index2.php
@@ -0,0 +1,4 @@
+<link rel="icon" href="./view/img/icono.png">
+<?php
+    include_once('./view/login.html');  
+?>
