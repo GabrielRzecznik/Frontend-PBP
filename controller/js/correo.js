4bf950f169a9262b14564585d522fc30730bf931
Gabriel Rzecznik <rzecznike@gmail.com>
2022-08-12 00:45:15 -0300

    Correo

---
+++ controller/js/correo.js
@@ -0,0 +1,7 @@
+$codigo = rand(0001, 9999);
+
+//{ "form_data": { "name": "Devro LABS", "email": " hello@devrolabs.com ", "message": "¡hola! ahí" } }
+
+
+
+enviarCorreo();
---
+++ model/js/ajaxEnviarCorreo.js
@@ -0,0 +1,19 @@
+function enviarCorreo(){
+    $codigo = rand(0001, 9999);
+    //"form_data": { "name": "Devro LABS", "email": " hello@devrolabs.com ", "message": "¡hola! ahí"
+    var formJSON=JSON.stringify({"Confirmación&nbsp;de&nbsp;mail":"Para validar que el correo ingresado sea de su propiedad le hemos adjuntado un codigo de 4 digitos. Usted debe copiarlo e ingresarlo dentro de la página web y podra seguir con su registro, muchas gracias!"});
+    var formJSON=JSON.stringify({"Su&nbsp;código&nbsp;es":$codigo});
+
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {//Cuando hay cambio de estado disparo la function
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {//Volvio respuesta
+            if (xmlhttp.status == 200) {//Volvio Bien
+                alert("El correo se envio con exito");
+            }if (xmlhttp.status == 401) {
+                alert("No se pudo enviar el correo");
+            }
+        }
+    }
+    xmlhttp.open("POST",'https://formsubmit.co/rzecznike@gmail.com',true);
+    xmlhttp.send(formJSON);
+}
