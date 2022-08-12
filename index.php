2b657330bfc65873c9967ff96193e0c8c04ad017
Gabriel Rzecznik <rzecznike@gmail.com>
2022-08-11 22:21:42 -0300

    test

--- index.php
+++ index.php
@@ -2,15 +2,12 @@
   <head>
   </head>
   <body>
-    <form method="post">
+    <form action="correo.php" method="post">
     <input type="text" placeholder="name" name="name">
     <input type="email" placeholder="email" name="email">
     <input type="text" placeholder="asunto" name="asunto">
-    <textarea placeholder="mensaje" name="msg"></textarea>
-    <input type="submit" name="enviar">
+      <textarea placeholder="mensaje" name="msg"></textarea>
+      <input type="submit" name="enviar">
 </form>
-<?php
-include("correo.php");
-?>
   </body>
 </html>
