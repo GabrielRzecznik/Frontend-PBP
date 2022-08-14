7e7f5a551f6108a33b35a8ccb0f6c17918a8169e
Gabriel Rzecznik <rzecznike@gmail.com>
2022-08-13 21:39:26 -0300

    Login

--- model/js/ajaxBuscarUsuario.js
+++ model/js/ajaxBuscarUsuario.js
@@ -6,19 +6,32 @@ function buscarUsuario(formulario){
     xmlhttp.onreadystatechange = function () {//Cuando hay cambio de estado disparo la function
         if (xmlhttp.readyState == XMLHttpRequest.DONE) {//Volvio respuesta
             if (xmlhttp.status == 200) {//Volvio Bien
-                //localStorage.setItem("correo", document.getElementById("correo").value);//Me guarda el correo
-                
                 //Tranforma la información que ataja en un JSON que puede ser leido mediante el data[][""]
                 var data=JSON.parse(xmlhttp.responseText);
-
-                localStorage.setItem("id_usuario", data[0]["id_usuario"]);//Me guarda el id_usuario de la bd
-                localStorage.setItem("id_paciente", data[0]["id_paciente"]);//Me guarda el id_usuario de la bd
-                localStorage.setItem("id_profesional", data[0]["id_profesional"]);//Me guarda el id_usuario de la bd
-                localStorage.setItem("nombreUsuario", data[0]["nombreUsuario"]);//Me guarda el nombreUsuario de la bd
-                localStorage.setItem("correo", data[0]["correo"]);//Me guarda el correo de la bd
                 
-                //Me redirige a la pagina principal
-                window.location.href = "https://frontend-pbp.herokuapp.com/view/inicioBusqueda.html";
+                if (data[0]["estadoUsuario"] == "Activo") {
+                    //Estado Usuario Activo
+                    localStorage.setItem("id_usuario", data[0]["id_usuario"]);
+                    localStorage.setItem("id_paciente", data[0]["id_paciente"]);
+                    localStorage.setItem("id_profesional", data[0]["id_profesional"]);
+                    localStorage.setItem("nombreUsuario", data[0]["nombreUsuario"]);
+                    localStorage.setItem("correo", data[0]["correo"]);
+                    //Redirigir
+                    window.location.href = "https://frontend-pbp.herokuapp.com/view/inicioBusqueda.html";
+                }if (data[0]["estadoUsuario"] == "En creación") {
+                    //Estado Usuario En creación
+                    localStorage.setItem("id_usuario", data[0]["id_usuario"]);
+                    //Redirigir
+                    window.location.href = "https://frontend-pbp.herokuapp.com/view/registroPerfil.html";
+                }if (data[0]["estadoUsuario"] == "Oculto") {
+                    //Estado Usuario Oculto
+                    alert("No se encontro el usuario!");
+                }
+                
+
+                
+                
+                
             }if (xmlhttp.status == 401) {
                 alert("No se encontro el usuario!");
                 document.getElementById('cargando').style.display = 'none';
