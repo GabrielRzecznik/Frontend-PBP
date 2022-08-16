2db123abf5930bb3cb0a9ae476f7fb9da7ae48aa
Gabriel Rzecznik <rzecznike@gmail.com>
2022-08-15 23:19:56 -0300

    test

--- controller/js/navegador.js
+++ controller/js/navegador.js
@@ -1,8 +1,6 @@
 //Verificar si hay logueo
 if (!localStorage.getItem("id_usuario")) {
     window.location.href = "https://parcial-edi-front.herokuapp.com/inicioBusqueda.html";
-}else{
-    document.getElementById('mostrar').style.display = 'block';
 }
 
 //Mostrar Usuario
