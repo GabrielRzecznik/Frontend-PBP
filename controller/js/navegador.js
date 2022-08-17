4540a0e6ee13bf687bc5bd9cbe114efdd6d04f68
Gabriel Rzecznik <rzecznike@gmail.com>
2022-08-17 03:00:03 -0300

    test

--- controller/js/navegador.js
+++ controller/js/navegador.js
@@ -109,8 +109,6 @@ formulario.addEventListener('submit', (e) => {
     
     e.preventDefault();//evita que se envien los datos y se refresque la pagina
     
-    console.log(nombreUsuarioValue);
-    console.log(contraseñaValue);
     if (nombreUsuarioValue === "") {
         alert("Usuario vacio");
     }if (contraseñaValue === "") {
