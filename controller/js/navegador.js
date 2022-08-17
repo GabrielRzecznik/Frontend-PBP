62af312f7e05b910f256dff09a7377e30e7073e7
Gabriel Rzecznik <rzecznike@gmail.com>
2022-08-17 01:33:23 -0300

    test

--- controller/js/navegador.js
+++ controller/js/navegador.js
@@ -1,5 +1,5 @@
 //Verificar si hay logueo
-if (localStorage.getItem("id_usuario") == null) {
+if (localStorage.getItem("id_usuario") == null && localStorage.getItem("nombreUsuario") == null) {
     window.location.href = "https://frontend-pbp.herokuapp.com/";
 }else{
     document.getElementById('mostrar').style.display = 'block';
