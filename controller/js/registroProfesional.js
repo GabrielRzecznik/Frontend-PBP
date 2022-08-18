3a771e9f3c19d9a092738d7dfdffa8fba1ea5594
Gabriel Rzecznik <rzecznike@gmail.com>
2022-08-17 22:52:26 -0300

    test

--- controller/js/registroProfesional.js
+++ controller/js/registroProfesional.js
@@ -244,7 +244,7 @@ if (especialidad.value == 0) {
 //Validar y Agregar Obras Sociales
 //Botón Agregar
 document.getElementById("agregar").addEventListener("click", function() {
-    document.getElementById('contenido').innerHTML='<span class="badge bg-secondary" id="1">Osde <button type="button" class="btn-close btn-close-white" aria-label="Close"></button></span>';
+    document.getElementById('contenido').innerHTML='<span class="badge bg-secondary" id="1">Osde <button type="button" class="btn-close btn-close-white" aria-label="Close" id="borrar"></button></span>';
 });
 //Botón Borrar
 document.getElementById("borrar").addEventListener("click", function() {
