a21032a1215bd5cdaee7318d8843db933ae532bd
Gabriel Rzecznik <rzecznike@gmail.com>
2022-08-19 02:36:10 -0300

    Registrar Profesional

--- model/js/ajaxRegistroProfesional.js
+++ model/js/ajaxRegistroProfesional.js
@@ -2,30 +2,26 @@ function registrarProfesional(formulario, tc, os){
     var formData= new FormData(formulario); //Las keys corresponden al atributo name de cada elemento  
     formData.append("tipoConsulta", tc);
     formData.append("obraSocial", os);
-    formData.append("id_paciente", localStorage.getItem("id_usuario"));
+    formData.append("id_usuario", localStorage.getItem("id_usuario"));
     var formJSON=JSON.stringify(Object.fromEntries(formData));
     console.log(formJSON);
 
     xmlhttp = new XMLHttpRequest();
 
-    /*
     xmlhttp.onreadystatechange = function () {//Cuando hay cambio de estado disparo la function
         if (xmlhttp.readyState == XMLHttpRequest.DONE) {//Volvio respuesta
             if (xmlhttp.status == 200) {
-                //var respuestaResivida = xmlhttp.responseText;
-                //alert(respuestaResivida);
-                
-                window.location.href = "https://frontend-pbp.herokuapp.com/view/seleccionRol.html";
-                
+                alert("Perfil profesional creado con existo!")
+                localStorage.clear();
+                window.location.href = "https://frontend-pbp.herokuapp.com/";
             }else if (xmlhttp.status == 500) {
-                alert("Ocurrio un error inesperado con el correo ingresado");
+                alert("Ocurrio un error inesperado!");
             }else{
                 alert("Fallo la conexión con el servidor!!");
             }
         }
     }
-    xmlhttp.open("POST",'https://backend-pbp.herokuapp.com/Pacientes/crearPaciente',true);
+    xmlhttp.open("POST",'https://backend-pbp.herokuapp.com/Profesionales/crearProfesional',true);
     xmlhttp.send(formJSON);
-    */
 }
 
