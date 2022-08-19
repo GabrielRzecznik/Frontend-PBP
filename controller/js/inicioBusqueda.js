202d72799d163750b1aec15a4018943242ffaf1a
Gabriel Rzecznik <rzecznike@gmail.com>
2022-08-19 11:43:26 -0300

    mapita test

--- controller/js/inicioBusqueda.js
+++ controller/js/inicioBusqueda.js
@@ -23,3 +23,4 @@ if (navigator.geolocation) {
 }
 
     
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1640.8724279365922!2d-58.354048612043975!3d-34.66114596650525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a33344a0818201%3A0x8499100d4daae3f5!2sLamadrid%20540%2C%20B1871DSF%20Crucecita%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1660919613213!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
