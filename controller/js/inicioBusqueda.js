f8de7dc7e6f3ce2eb4a3ad6304fd70ba8323e4be
Gabriel Rzecznik <rzecznike@gmail.com>
2022-08-21 03:28:08 -0300

    test

--- controller/js/inicioBusqueda.js
+++ controller/js/inicioBusqueda.js
@@ -11,5 +11,89 @@ mostrarMapa2.innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!1m1
 //Lamadrid%20540%2C%20B1871DSF%20Crucecita%2C%20Provincia%20de%20Buenos%20Aires - DATOS SENSIBLES
 //Av.%20Eduardo%20Madero%2C%20Buenos%20Aires - DATOS SENSIBLES
 
+//#region Validación de Campos
+const inputs = document.querySelectorAll('#formulario input');
 
+const expresiones = {
+    obraSocial: /^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü/ 0-9\s]{2,40}$/
+};
 
+const campos = {
+    obraSocial: false
+};
+
+const validarFormulario = (e) => {
+    switch (e.target.name) {
+        case 'obraSocial':
+            if (expresiones.obraSocial.test(e.target.value)) {
+                document.getElementById('iconoObraSocial').classList.remove('bi-exclamation-circle-fill','signo','bi-x-circle-fill','noValidado');//Borrar !,x
+                document.getElementById('iconoObraSocial').classList.add('mostrar','bi-check-circle-fill','validado');//Mostrar,✓,"Verde"
+                //Alerta de error
+                document.getElementById('alertObraSocial').classList.remove('alertaError');
+                //Validar campo
+                campos['obraSocial'] = true;
+            }else if(obraSocial.value.trim() == ""){
+                document.getElementById('iconoObraSocial').classList.remove('mostrar','bi-check-circle-fill','validado','bi-exclamation-circle-fill','signo','bi-x-circle-fill','noValidado');
+                //Limpiar 
+                document.getElementById('alertObraSocial').classList.remove('alertaError');
+            }else{
+                document.getElementById('iconoObraSocial').classList.remove('bi-check-circle-fill','validado','bi-exclamation-circle-fill','signo');
+                document.getElementById('iconoObraSocial').classList.add('mostrar','bi-x-circle-fill','noValidado');
+                //Mensaje de error
+                document.getElementById('alertObraSocial').classList.add('alertaError');
+                //Limpiar 
+                document.getElementById('alertMatricula').classList.remove('alertaError');
+                document.getElementById('alertTipoConsulta').classList.remove('alertaError');
+                document.getElementById('alertProvinciaConsultorio').classList.remove('alertaError');
+                document.getElementById('alertLocalidadConsultorio').classList.remove('alertaError');
+                document.getElementById('alertCalleConsultorio').classList.remove('alertaError');
+                document.getElementById('alertAlturaConsultorio').classList.remove('alertaError');
+                document.getElementById('alertDepartamentoConsultorio').classList.remove('alertaError');
+                campos['obraSocial'] = false;
+            }
+        break
+    }
+};
+
+//#endregion
+
+//#region Select Especialidad
+document.getElementById("especialidad").addEventListener('change', (event) => {
+    if (event.target.value != 0) {
+        document.getElementById('iconoEspecialidad').classList.remove('signo','noValidado','bi-exclamation-circle-fill','bi-x-circle-fill');
+        document.getElementById('iconoEspecialidad').classList.add('validado','bi-check-circle-fill');
+        //Mensaje de error
+        document.getElementById('alertEspecialidad').classList.remove('alertaError');
+        //Validar
+        campos['especialidad'] = true;
+    }else{
+        document.getElementById('iconoEspecialidad').classList.remove('signo','validado','bi-exclamation-circle-fill','bi-check-circle-fill');
+        document.getElementById('iconoEspecialidad').classList.add('noValidado','bi-x-circle-fill');
+        //Mensaje de error
+        document.getElementById('alertEspecialidad').classList.add('alertaError');
+        //Limpiar mensaje
+        document.getElementById('alertMatricula').classList.remove('alertaError');
+        document.getElementById('alertObraSocial').classList.remove('alertaError');
+        document.getElementById('alertTipoConsulta').classList.remove('alertaError');
+        document.getElementById('alertProvinciaConsultorio').classList.remove('alertaError');
+        document.getElementById('alertLocalidadConsultorio').classList.remove('alertaError');
+        document.getElementById('alertCalleConsultorio').classList.remove('alertaError');
+        document.getElementById('alertAlturaConsultorio').classList.remove('alertaError');
+        document.getElementById('alertDepartamentoConsultorio').classList.remove('alertaError');
+        campos['especialidad'] = false;
+    }
+});
+
+if (especialidad.value == 0) {
+    document.getElementById('iconoEspecialidad').classList.add('mostrar');//Agregar
+    document.getElementById('iconoEspecialidad').classList.remove('bi-check-circle-fill');//Borrar
+    campos['especialidad'] = false;
+}
+//#endregion
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup' , validarFormulario);//cuando levanto la tecla se ejecuta un codigo
+    input.addEventListener('blur' , validarFormulario);//cuando me salgo y preciono fuera del input
+});
+
+//#endregion
--- model/js/ajaxBuscarProfesionales.js
+++ model/js/ajaxBuscarProfesionales.js
@@ -1,26 +1,25 @@
-function buscarUsuarioDeshabilitado(correo){
-    var formData= new FormData();
-    formData.append("correo", correo);
+function buscarProfesionales(formulario){
+    var formData= new FormData(formulario);
+    //Agregar id_profesional para que no se muestre el mismo profesional que realiza la busqueda - agregar en el login
     var formJSON=JSON.stringify(Object.fromEntries(formData));
 
     xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {//Cuando hay cambio de estado disparo la function
         if (xmlhttp.readyState == XMLHttpRequest.DONE) {//Volvio respuesta
             if (xmlhttp.status == 200) {//Volvio Bien
-                //console.log("Usuario encontrado");
-                //Enviar correo
-                enviarCorreo(correo);
+                //Creación de cartas profesionales
+
                 //Cambiar formulario
-                document.getElementById('form1').style.display = 'none';
-                document.getElementById('form2').style.display = 'block';
+                document.getElementById('cargandoBuscar').style.display = 'none';
+                document.getElementById('tituloBuscar').style.display = 'block';
             }if (xmlhttp.status == 401) {
-                alert("!No se encontro el usuario!");
+                alert("!No se encontron profesionales!");
                 //Corregir error sppiner
                 document.getElementById('cargandoBuscar').style.display = 'none';
                 document.getElementById('tituloBuscar').style.display = 'block';
             }
         }
     }
-    xmlhttp.open("POST",'https://backend-pbp.herokuapp.com/Usuarios/buscarUsuarioDeshabilitado',true);
+    xmlhttp.open("POST",'https://backend-pbp.herokuapp.com/Profesional/buscarProfesionales',true);
     xmlhttp.send(formJSON);
 }
