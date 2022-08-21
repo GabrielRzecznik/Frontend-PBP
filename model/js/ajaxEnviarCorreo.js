1efe09721bb09549dd5075d173068159a95e8020
Gabriel Rzecznik <rzecznike@gmail.com>
2022-08-21 17:17:05 -0300

    test

--- model/js/ajaxEnviarCorreo.js
+++ model/js/ajaxEnviarCorreo.js
@@ -14,7 +14,7 @@ function enviarCorreo(correo, asignarDuracion){
         if (xmlhttp.readyState == XMLHttpRequest.DONE) {
             if (xmlhttp.status == 200) {
                 if (asignarDuracion) {
-                    const tiempo = setTimeout(anularCodigo, 5000);//10 minutos = 600000
+                    const tiempo = setTimeout(anularCodigo, 150000);//10 minutos = 600000
             
                     function anularCodigo() {
                         alert("Código vencido! Han pasado 10 minutos!");
