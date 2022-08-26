46ec740d1844fd8b7850d5a896d16c145dee2896
Gabriel Rzecznik <rzecznike@gmail.com>
2022-08-25 21:09:30 -0300

    test

--- model/js/ajaxBuscarProfesionales.js
+++ model/js/ajaxBuscarProfesionales.js
@@ -10,6 +10,32 @@ function buscarProfesionales(formulario){
             if (xmlhttp.status == 200) {//Volvio Bien
                 //Creación de cartas profesionales
 
+                var data = JSON.parse(xmlhttp.responseText);
+                var nombre_columnas = Object.keys(data[0]);
+
+                for (var i = 0; i < nombre_columnas.length; i++) {
+                    colum.innerHTML = colum.innerHTML + '<th scope="col">' + nombre_columnas[i] + '</th>';
+                }
+
+                //colum.innerHTML = colum.innerHTML + '<th scope="col">Editar</th>' + '<th scope="col">Borrar</th>' + '<th scope="col">Actualizar</th>';
+
+                for (var i = 0; i < data.length; i++) {
+                    con.innerHTML = con.innerHTML    
+                    + '<span>' + data[i].especialidad + '</span>' 
+                    + '<span>' + data[i].matricula + '</span>'
+                    + '<span>' + data[i].calleConsultorio + '</span>'
+                    + '<span>' + data[i].alturaConsultorio + '</span>'
+                    + '<span>' + data[i].color + '</span>'
+                    + '<span>' + data[i].estado + '</span>'
+                    + '<span>' + data[i].cambio + '</span>'
+                    + '<span>' + data[i].combustible + '</span>'
+                    + '<span>' + data[i].valor + '</span>'
+                    + '<span>' + data[i].kilometraje + '</span>'
+                    + '<span>' + data[i].anio + '</span>'
+                    + '<span>' + data[i].propietario + '</span>'                                    
+                }
+
+
                 //Cambiar formulario
                 document.getElementById('cargandoBuscar').style.display = 'none';
                 document.getElementById('tituloBuscar').style.display = 'block';
