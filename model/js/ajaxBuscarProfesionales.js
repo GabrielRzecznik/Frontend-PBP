db93f142491423e51488dafe832762d41e635cd8
Gabriel Rzecznik <rzecznike@gmail.com>
2022-08-26 01:30:46 -0300

    test

--- model/js/ajaxBuscarProfesionales.js
+++ model/js/ajaxBuscarProfesionales.js
@@ -34,7 +34,7 @@ function buscarProfesionales(formulario, $valorUbicacion){
                 var data = JSON.parse(xmlhttp.responseText);
 
                 //colum.innerHTML = colum.innerHTML + '<th scope="col">Editar</th>' + '<th scope="col">Borrar</th>' + '<th scope="col">Actualizar</th>';
-                console.log(data);
+                console.log(data.length);
                 for (var i = 0; i < data.length; i++) {
                     con.innerHTML = "<span>Hola</span>"
                     //con.innerHTML +
