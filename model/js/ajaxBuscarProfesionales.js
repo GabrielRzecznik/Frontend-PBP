bb59d3de033a956cc381a93799f28b58171d533b
Gabriel Rzecznik <rzecznike@gmail.com>
2022-08-28 15:28:40 -0300

    test

--- model/js/ajaxBuscarProfesionales.js
+++ model/js/ajaxBuscarProfesionales.js
@@ -86,8 +86,8 @@ function buscarProfesionales(formulario, $valorUbicacion){
                     */
                     //Mostrar Mapa
                     $calleAvenida = data[i].calleConsultorio;
-                    $calleAvenida = $calleAvenida.replace(/calle/,'AEM%2C%20C.');
-                    $calleAvenida = $calleAvenida.replace(/Calle/,'AEM%2C%20C.');
+                    $calleAvenida = $calleAvenida.replace(/calle/,'C.');
+                    $calleAvenida = $calleAvenida.replace(/Calle/,'C.');
                     $calleAvenida = $calleAvenida.replace(/avenida/,'Av.');
                     $calleAvenida = $calleAvenida.replace(/Avenida/,'Av.');
                     $calleAvenida = $calleAvenida.replace(/ /,'%20');
@@ -108,9 +108,10 @@ function buscarProfesionales(formulario, $valorUbicacion){
                     
                     $localidad = data[i].localidadConsultorio;
                     $localidad = $localidad.replace(/ /,'%20');
-                    
+
                     $provincia = data[i].provinciaConsultorio;//"Provincia%20Buenos%20Aires"
                     $provincia = $provincia.replace(/ /,'%20');
+                    $provincia = 'Provincia%20'+$provincia;
 
 
                     document.getElementById('visualizarMapa'+i).innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1640.8724279365922!2d-58.354048612043975!3d-34.66114596650525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s!2s'+$calleAvenida+'%2'+$altura+'%2C%20'+$codigoPostal+'DSF%20'+$localidad+'%2C%20'+$provincia+'!5e0!3m2!1ses-419!2sar" class="mapa" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
