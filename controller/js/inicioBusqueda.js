9be041d20be58935856ff7d2631f57f03137b7cb
Gabriel Rzecznik <rzecznike@gmail.com>
2022-08-28 15:21:46 -0300

    test

--- controller/js/inicioBusqueda.js
+++ controller/js/inicioBusqueda.js
@@ -1,12 +1,12 @@
-var mostrarMapa = document.getElementById("visualizarMapa");
+//var mostrarMapa = document.getElementById("visualizarMapa");
 //var mostrarMapa2 = document.getElementById("visualizarMapa2");
-$calleAvenida = "C.%2045";
-$altura = "0986";//0540
-$codigoPostal = "B1871";//B1871
-$localidad = "La%20Plata";
-$provincia = "Provincia%20Buenos%20Aires";
+//$calleAvenida = "C.%2045";
+//$altura = "0986";//0540
+//$codigoPostal = "B1871";//B1871
+//$localidad = "La%20Plata";
+//$provincia = "Provincia%20Buenos%20Aires";
 
-mostrarMapa.innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1640.8724279365922!2d-58.354048612043975!3d-34.66114596650525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s!2s'+$calleAvenida+'%2'+$altura+'%2C%20'+$codigoPostal+'DSF%20'+$localidad+'%2C%20'+$provincia+'!5e0!3m2!1ses-419!2sar" class="mapa" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+//mostrarMapa.innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1640.8724279365922!2d-58.354048612043975!3d-34.66114596650525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s!2s'+$calleAvenida+'%2'+$altura+'%2C%20'+$codigoPostal+'DSF%20'+$localidad+'%2C%20'+$provincia+'!5e0!3m2!1ses-419!2sar" class="mapa" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
 //mostrarMapa2.innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1640.8724279365922!2d-58.354048612043975!3d-34.66114596650525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s!2s'+$calleAvenida+'%2'+$altura+'%2C%20'+$codigoPostal+'DSF%20'+$localidad+'%2C%20'+$provincia+'!5e0!3m2!1ses-419!2sar" class="mapa" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
 //Lamadrid%20540%2C%20B1871DSF%20Crucecita%2C%20Provincia%20de%20Buenos%20Aires - DATOS SENSIBLES
 //Av.%20Eduardo%20Madero%2C%20Buenos%20Aires - DATOS SENSIBLES
