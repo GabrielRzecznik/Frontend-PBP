583360def9feca31c009db2b80d6ba7a04f9906a
Gabriel Rzecznik <rzecznike@gmail.com>
2022-08-28 13:56:03 -0300

    test

--- model/js/ajaxBuscarProfesionales.js
+++ model/js/ajaxBuscarProfesionales.js
@@ -36,6 +36,15 @@ function buscarProfesionales(formulario, $valorUbicacion){
                 //colum.innerHTML = colum.innerHTML + '<th scope="col">Editar</th>' + '<th scope="col">Borrar</th>' + '<th scope="col">Actualizar</th>';
                 console.log(data);
                 for (var i = 0; i < data.length; i++) {//data.length undefined, recorrer como foreach
+                    //Mapa
+                    $calleAvenida = data[i].calleConsultorio;
+                    $altura = data[i].alturaConsultorio;//0540
+                    //$codigoPostal = "B2756";//B1871
+                    $localidad = data[i].localidadConsultorio;
+                    $provincia = data[i].provinciaConsultorio;//"Provincia%20Buenos%20Aires"
+                    mostrarMapa.innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1640.8724279365922!2d-58.354048612043975!3d-34.66114596650525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s!2s'+$calleAvenida+'%2'+$altura+'%2C%20'+$codigoPostal+'DSF%20'+$localidad+'%2C%20'+$provincia+'!5e0!3m2!1ses-419!2sar" class="mapa" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+                    
+                    //Carta Profesional
                     con.innerHTML = con.innerHTML +
                     '<div class="col-md-6">' +
                         '<div class="card bg-light carta">' +
@@ -68,7 +77,7 @@ function buscarProfesionales(formulario, $valorUbicacion){
                                     '</div>' +
                                 '</div>' +
                                 '<div class="col-6">' +
-                                    '<span id="visualizarMapa2"></span>' + //Iterar id
+                                    '<span id="visualizarMapa'+i+'"></span>' + //Iterar id
                                 '</div>' +
                             '</div>' +
                         '</div>' +
