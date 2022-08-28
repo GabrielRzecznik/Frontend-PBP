26df71a0d599fe4f7cf3a09c7ee197c87cdca856
Gabriel Rzecznik <rzecznike@gmail.com>
2022-08-28 12:22:01 -0300

    test

--- model/js/ajaxRegistroUsuario.js
+++ model/js/ajaxRegistroUsuario.js
@@ -11,7 +11,7 @@ function registrarUsuario(formulario){
                 localStorage.setItem("id_usuario", data[0]["id_usuario"]);//Me guarda el id_usuario de la bd
                 window.location.href = "https://frontend-pbp.herokuapp.com/view/registroPerfil.html";
             }else if (xmlhttp.status == 500) {
-                alert("¡Ocurrio un error inesperado con el correo ingresado!");
+                alert("¡Ocurrio un error inesperado!");
                 //Marcar Correo
                 document.getElementById('iconoCorreo').classList.remove('validado');
                 document.querySelector('#iconoCorreo').classList.add('bi-x-circle-fill');
