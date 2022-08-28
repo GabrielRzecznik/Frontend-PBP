8b4ce2c46958d8eeffe34130a17c6a6646356ea4
Gabriel Rzecznik <rzecznike@gmail.com>
2022-08-28 14:37:25 -0300

    test

--- model/js/ajaxBuscarProfesionales.js
+++ model/js/ajaxBuscarProfesionales.js
@@ -78,19 +78,11 @@ function buscarProfesionales(formulario, $valorUbicacion){
                         '</div>' +
                     '</div>';
                     //Mostrar Mapa
-                    $calleAvenida = data[i].calleConsultorio;
-                    $calleAvenida = $calleAvenida.replace(/ /,'%20');
-
-                    $altura = data[i].alturaConsultorio;//0540
-                    $altura = $altura.replace(/ /,'%20');
-
-                    $codigoPostal = "B1871";//B1871 Analizar si vale la pena incorporar segun el impacto
-                    
-                    $localidad = data[i].localidadConsultorio;
-                    $localidad = $localidad.replace(/ /,'%20');
-                    
-                    $provincia = data[i].provinciaConsultorio;//"Provincia%20Buenos%20Aires"
-                    $provincia = $provincia.replace(/ /,'%20');
+                    $calleAvenida = "Varela";
+$altura = "0096";//0540
+$codigoPostal = "B2756";//B1871
+$localidad = "Avellaneda";
+$provincia = "Provincia%20Buenos%20Aires";
 
 
                     document.getElementById('visualizarMapa'+i).innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1640.8724279365922!2d-58.354048612043975!3d-34.66114596650525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s!2s'+$calleAvenida+'%2'+$altura+'%2C%20'+$codigoPostal+'DSF%20'+$localidad+'%2C%20'+$provincia+'!5e0!3m2!1ses-419!2sar" class="mapa" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
