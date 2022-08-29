0f5d2783c25ec04bd1f5dbb9d3ac66f6c773a922
Gabriel Rzecznik <rzecznike@gmail.com>
2022-08-29 10:14:39 -0300

    test

--- model/js/ajaxBuscarProvincias.js
+++ model/js/ajaxBuscarProvincias.js
@@ -1,4 +1,4 @@
-let prov = document.getElementById('prov');
+
 
 function buscarProvincias(){
     xmlhttp = new XMLHttpRequest();
@@ -8,8 +8,8 @@ function buscarProvincias(){
                 var data=JSON.parse(xmlhttp.responseText);
                 console.log(data["provincias"][0]["nombre"]);
                 console.log(data["provincias"].length);
+                let prov = document.getElementById('prov');
                 for (var i = 0; i < data["provincias"].length; i++) {
-                    
                     prov.innerHTML += '<option value="'+ data["provincias"][i]["nombre"]+'">'+data["provincias"][i]["nombre"]+'</option>';
                 }
             }if (xmlhttp.status == 401) {
