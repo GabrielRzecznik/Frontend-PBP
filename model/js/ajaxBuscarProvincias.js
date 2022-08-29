115cd585014e9c321ffe2b44327b39384d4cda67
Gabriel Rzecznik <rzecznike@gmail.com>
2022-08-29 02:28:12 -0300

    test

--- model/js/ajaxBuscarProvincias.js
+++ model/js/ajaxBuscarProvincias.js
@@ -6,12 +6,11 @@ function buscarProvincias(){
         if (xmlhttp.readyState == XMLHttpRequest.DONE) {
             if (xmlhttp.status == 200) {
                 var data=JSON.parse(xmlhttp.responseText);
-                selectProv.innerHTML = "";
                 console.log(data["provincias"][0]["nombre"]);
                 console.log(data["provincias"].length);
                 for (var i = 0; i < data["provincias"].length; i++) {
                     console.log("Hola");
-                    selectProv.innerHTML = selectProv.innerHTML + '<option value="'+ data["provincias"][i]["nombre"]+'">'+data[i]+'</option>'
+                    selectProv.innerHTML += '<option value="'+ data["provincias"][i]["nombre"]+'">'+data[i]+'</option>'
                 }
             }if (xmlhttp.status == 401) {
                 alert("Ocurrio un error inesperado al traer las provincias")
