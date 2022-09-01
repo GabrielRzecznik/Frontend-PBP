8c3101219390f121d92f0ac3345876d5397dce42
Gabriel Rzecznik <rzecznike@gmail.com>
2022-09-01 18:17:00 -0300

    test

--- controller/js/registroProfesional.js
+++ controller/js/registroProfesional.js
@@ -445,7 +445,6 @@ formulario.addEventListener('submit', (e) => {
     }else{
         if (especialidadValue === "0" || matriculaValue === "" || provinciaConsultorioValue === "0") {
             alert("¡Debe completar todos los campos obligatorios!");
-            campos['provinciaConsultorio'] = true;
         }
     }
 
