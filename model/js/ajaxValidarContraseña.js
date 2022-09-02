3ba4160805919966416c34829aa024fdd17760bc
Gabriel Rzecznik <rzecznike@gmail.com>
2022-09-01 23:17:00 -0300

    test

--- model/js/ajaxValidarContraseña.js
+++ model/js/ajaxValidarContraseña.js
@@ -1,25 +1,32 @@
 function buscarUsuario(formulario){
     var formData = new FormData(formulario);
+    if (localStorage.getItem("id_usuario") != "") {
+        formData.append("correo", localStorage.getItem("correo"));
+    }
     var formJSON = JSON.stringify(Object.fromEntries(formData));
-    //console.log(formJSON);
+
     xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {//Cuando hay cambio de estado disparo la function
-        if (xmlhttp.readyState == XMLHttpRequest.DONE) {//Volvio respuesta
-            if (xmlhttp.status == 200) {//Volvio Bien
-                //Tranforma la información que ataja en un JSON que puede ser leido mediante el data[][""]
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+            if (xmlhttp.status == 200) {
+                
                 var data=JSON.parse(xmlhttp.responseText);
                 
                 if (data[0]["estadoUsuario"] == "Activo") {
                     //Estado Usuario Activo
-                    localStorage.setItem("id_usuario", data[0]["id_usuario"]);
-                    localStorage.setItem("id_paciente", data[0]["id_paciente"]);
-                    localStorage.setItem("id_profesional", data[0]["id_profesional"]);
-                    localStorage.setItem("correo", data[0]["correo"]);
-                    localStorage.setItem("nombreUsuario", data[0]["nombreUsuario"]);
-                    localStorage.setItem("latitud", data[0]["latitud"]);
-                    localStorage.setItem("longitud", data[0]["longitud"]);
-                    //Redirigir
-                    window.location.href = "https://frontend-pbp.herokuapp.com/view/inicioBusqueda.html";
+                    if (localStorage.getItem("id_usuario") != "") {
+                        localStorage.setItem("id_usuario", data[0]["id_usuario"]);
+                        localStorage.setItem("id_paciente", data[0]["id_paciente"]);
+                        localStorage.setItem("id_profesional", data[0]["id_profesional"]);
+                        localStorage.setItem("correo", data[0]["correo"]);
+                        localStorage.setItem("nombreUsuario", data[0]["nombreUsuario"]);
+                        localStorage.setItem("latitud", data[0]["latitud"]);
+                        localStorage.setItem("longitud", data[0]["longitud"]);
+                        //Redirigir
+                        window.location.href = "https://frontend-pbp.herokuapp.com/view/inicioBusqueda.html";
+                    }else{
+                        
+                    }
                 }if (data[0]["estadoUsuario"] == "En Creación") {
                     //Estado Usuario En creación
                     localStorage.setItem("id_usuario", data[0]["id_usuario"]);
