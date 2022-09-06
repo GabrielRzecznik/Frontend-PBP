238a8bb079d797ed6ba7692cd477302a0cd72009
Gabriel Rzecznik <rzecznike@gmail.com>
2022-09-06 01:01:55 -0300

    test

--- model/js/ajaxEditarPaciente.js
+++ model/js/ajaxEditarPaciente.js
@@ -1,5 +1,7 @@
-function editarPaciente(formulario){
+function editarPaciente(formulario, $latitud, $longitud){
     var formData = new FormData(formulario);
+    formData.append("latitud", $latitud);
+    formData.append("longitud", $longitud);
     formData.append("id_usuario", localStorage.getItem("id_usuario"));
     var formJSON = JSON.stringify(Object.fromEntries(formData));
     console.log(formJSON);
