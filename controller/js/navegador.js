4c5d7fd185434e82d0f37976a8020f57a1c5b0af
Gabriel Rzecznik <rzecznike@gmail.com>
2022-09-07 00:52:25 -0300

    test

--- controller/js/navegador.js
+++ controller/js/navegador.js
@@ -18,7 +18,7 @@ document.getElementById('apellido').value = localStorage["apellido"];
 document.getElementById('fechaNacimiento').value = localStorage["fechaNacimiento"];
 document.getElementById('sexoPaciente').value = localStorage["sexo"];
 document.getElementById('telefono').value = localStorage["telefono"];
-document.getElementById('provincia').value = localStorage["provincia"];
+
 document.getElementById('localidad').value = localStorage["localidad"];
 document.getElementById('calle').value = localStorage["calle"];
 document.getElementById('altura').value = localStorage["altura"];
