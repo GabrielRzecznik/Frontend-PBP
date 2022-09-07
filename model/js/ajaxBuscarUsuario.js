1521a123b325066d89b7a93833bb33ed3f6fa18c
Gabriel Rzecznik <rzecznike@gmail.com>
2022-09-07 00:15:00 -0300

    test

--- model/js/ajaxBuscarUsuario.js
+++ model/js/ajaxBuscarUsuario.js
@@ -14,6 +14,17 @@ function buscarUsuario(formulario){
                     localStorage.setItem("id_usuario", data[0]["id_usuario"]);
                     localStorage.setItem("id_paciente", data[0]["id_paciente"]);
                     localStorage.setItem("id_profesional", data[0]["id_profesional"]);
+                    localStorage.setItem("nombre", data[0]["nombre"]);
+                    localStorage.setItem("apellido", data[0]["apellido"]);
+                    localStorage.setItem("fechaNacimiento", data[0]["fechaNacimiento"]);
+                    localStorage.setItem("sexo", data[0]["sexo"]);
+                    localStorage.setItem("foto", data[0]["foto"]);
+                    localStorage.setItem("telefono", data[0]["telefono"]);
+                    localStorage.setItem("provincia", data[0]["provincia"]);
+                    localStorage.setItem("localidad", data[0]["localidad"]);
+                    localStorage.setItem("calle", data[0]["calle"]);
+                    localStorage.setItem("altura", data[0]["altura"]);
+                    localStorage.setItem("departamento", data[0]["departamento"]);
                     localStorage.setItem("correo", data[0]["correo"]);
                     localStorage.setItem("nombreUsuario", data[0]["nombreUsuario"]);
                     localStorage.setItem("latitud", data[0]["latitud"]);
