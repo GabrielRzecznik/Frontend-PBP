8a38e1d1a9ef6a338dbec8dd7b8735fe1ce96737
Gabriel Rzecznik <rzecznike@gmail.com>
2022-09-09 09:20:37 -0300

    test

---
+++ controller/php/enviarCorreo.php
@@ -0,0 +1,44 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
+
+$mail = new PHPMailer(true);
+
+try {
+    //Configuración del servidor
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                    
+    $mail->isSMTP();                                         
+    $mail->Host       = 'mail.trebolnetsi.com';                  //Setear el servidor SMTP
+    $mail->SMTPAuth   = true;                                   //autenticación SMTP
+    $mail->Username   = 'franco.zuarez@trebolnetsi.com';                     //SMTP usuario
+    $mail->Password   = 'fran1357F//**';                               //SMTP contraseña
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;          
+    $mail->Port       = 465;                                    //puerto 
+
+    //Recipientes
+    $mail->setFrom('francozuarez1@gmail.com', 'Profesional By Proximity');  //Desde donde se envía el mensaje
+    $mail->addAddress('rzecznike@gmail.com', 'Prueba');     //Hacia donde
+    //$mail->addAddress('Profesional By Proximity');          
+   // $mail->addReplyTo('info@example.com', 'Information');
+    //$mail->addCC('cc@example.com');
+    //$mail->addBCC('bcc@example.com');
+
+    //Imagen
+    $mail->addAttachment('https://frontend-pbp.herokuapp.com/view/img/icono.png');         //Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Contenido
+    $mail->isHTML(true);                                  //Se puede setear en HTML
+    $mail->Subject = 'Profesional By Proximity';
+    $mail->Body    = 'Se ha enviado con exito la prueba';
+   // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'El mail ha sido enviado';
+} catch (Exception $e) {
+    echo "El mail no ha sido enviado. Mailer Error: {$mail->ErrorInfo}";
+}
---
+++ controller/php/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'd1d79575343ac2685ff549c9ca8cde0839520bd0',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'd1d79575343ac2685ff549c9ca8cde0839520bd0',
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.6.4',
+            'version' => '6.6.4.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'reference' => 'a94fdebaea6bd17f51be0c2373ab80d3d681269b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
