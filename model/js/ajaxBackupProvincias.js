c9de1db46c27ecffbf53b90cfd072b9a97029dec
Gabriel Rzecznik <rzecznike@gmail.com>
2022-09-16 00:15:25 -0300

    test

--- model/js/ajaxBackupProvincias.js
+++ model/js/ajaxBackupProvincias.js
@@ -22,7 +22,9 @@ function mostrarBackupProvincias(){
     xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == XMLHttpRequest.DONE) {
-            if (xmlhttp.status == 401) {
+            if (xmlhttp.status == 200) {
+                //Seleccionar caso mediante parametro
+             }if (xmlhttp.status == 401) {
                alert("¡Ocurrio un error inesperado!");
             }else{
                 alert("¡Revise su conexión con internet!")
