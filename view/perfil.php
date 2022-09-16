87fb248a55dcdebf61d44facd5794ab4f8bf52e3
Gabriel Rzecznik <rzecznike@gmail.com>
2022-09-16 06:32:52 -0300

    test

--- view/perfil.php
+++ view/perfil.php
@@ -101,6 +101,8 @@
 			</div>
 	</div>
 	<br>
+	<script src="../model/js/ajaxBackupProvincias.js"></script>
+	<script src="../model/js/ajaxMostrarBackupProvincias.js"></script>
 	<script src="../model/js/ajaxBuscarPerfil.js"></script>
     <script src="../controller/js/perfil.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
