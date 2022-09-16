c4bf87f3d5a69a48fb3666e1c8640ead5f3af814
Gabriel Rzecznik <rzecznike@gmail.com>
2022-09-16 01:02:00 -0300

    test

--- model/js/ajaxBackupProvincias.js
+++ model/js/ajaxBackupProvincias.js
@@ -2,15 +2,12 @@ function backupProvincias(provincias){
     var formData = new FormData();
     formData.append("jsonProvincias", '{'+provincias+'}');
     var formJSON = JSON.stringify(Object.fromEntries(formData));
-    console.log(formJSON);
 
     xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == XMLHttpRequest.DONE) {
             if (xmlhttp.status == 401) {
                alert("¡Ocurrio un error inesperado!");
-            }else{
-                alert("¡Revise su conexión con internet!")
             }
         }
     }
