0e2d3329f69cad05016caacb1e5854fdfc15ac1f
Gabriel Rzecznik <rzecznike@gmail.com>
2022-09-18 20:07:37 -0300

    test

--- model/js/ajaxCrearSolicitud.js
+++ model/js/ajaxCrearSolicitud.js
@@ -6,10 +6,13 @@ function crearSolicitud(seleccionConsulta, obraSocialSolicitud, horaDesdeSolicit
     xmlhttp.onreadystatechange = function () {//Cuando hay cambio de estado disparo la function
         if (xmlhttp.readyState == XMLHttpRequest.DONE) {//Volvio respuesta
             if (xmlhttp.status == 200) {//Volvio Bien
+                document.getElementById('confirmarSolicitud').style.display = 'block';
+                document.getElementById('confSoliCarg').style.display = 'none';
                 alert("¡Solicitud enviada con exito!");
-
                 location.reload();
             }else{
+                document.getElementById('confirmarSolicitud').style.display = 'block';
+                document.getElementById('confSoliCarg').style.display = 'none';
                 alert("¡Ocurrio un error inesperado!");
             }
         }
