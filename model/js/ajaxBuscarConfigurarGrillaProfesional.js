3a8b2425807b277b4e5b02dc866b738da2a5d998
Gabriel Rzecznik <rzecznike@gmail.com>
2022-09-21 15:01:00 -0300

    test

--- model/js/ajaxBuscarConfigurarGrillaProfesional.js
+++ model/js/ajaxBuscarConfigurarGrillaProfesional.js
@@ -9,7 +9,12 @@ function configurarGrillaProfesional($id_profesional){
     xmlhttp.onreadystatechange = function () {//Cuando hay cambio de estado disparo la function
         if (xmlhttp.readyState == XMLHttpRequest.DONE) {//Volvio respuesta
             if (xmlhttp.status == 200) {
-               
+                var data=JSON.parse(xmlhttp.responseText);
+                $duracionConsulta = data[0]["duracionConsulta"];
+                $descanso = data[0]["descanso"];
+                $rangoHorarioDiaDesde = data[0]["rangoHorarioDiaDesde"];
+                $rangoHorarioDiaHasta = data[0]["rangoHorarioDiaHasta"];
+                $diasAtencion = data[0]["diasAtencion"];
             }else{
                 alert("¡Ocurrio un error inesperado al mostrar la configuración de la grilla profesional!");
             }
