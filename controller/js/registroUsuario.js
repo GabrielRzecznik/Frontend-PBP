925a9cc0f86951c80b799453c1ea737c559d3d25
Gabriel Rzecznik <rzecznike@gmail.com>
2022-09-23 10:19:07 -0300

    test

--- controller/js/registroUsuario.js
+++ controller/js/registroUsuario.js
@@ -127,6 +127,10 @@ formulario.addEventListener('submit', (e) => {
         alert("¡Las contraseñas no coinciden!")
     }
     
+    if (campos.nombreUsuario == false || campos.correo == false || campos.password == false || campos.password2 == false) {
+        alert("Error al ingresar los datos: ¡Formato no valido, verifique los mismos e intente nuevamente!.");
+    }
+
     if (campos.correo && campos.password && (passwordValue === password2Value)) {
         //Enviar AJAX
         document.getElementById('tituloBuscar').style.display = 'none';
