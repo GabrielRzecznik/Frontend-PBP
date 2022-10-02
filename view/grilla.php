a7c92849768dadfa724157c39ff422cc71b946eb
Gabriel Rzecznik <rzecznike@gmail.com>
2022-10-02 18:55:12 -0300

    test

--- view/grilla.php
+++ view/grilla.php
@@ -25,8 +25,7 @@
 	<br>
 	<div class="container">
 		<div class="row">
-			<div class="col-2"></div>
-			<div class="col-8">
+			<div class="col-12">
 				<div class="card text-dark bg-light contenedorFullCalendar">
 					<div class="card-body">
 						<div id='calendar'>
@@ -34,7 +33,6 @@
 					</div>
 				</div>
 			</div>
-			<div class="col-2"></div>
 		</div>
 		
 	</div>
