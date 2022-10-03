3b46f8046cbd44708d5c8eb7ae573213c3f9d59e
Gabriel Rzecznik <rzecznike@gmail.com>
2022-10-03 02:35:24 -0300

    test

--- view/grilla.php
+++ view/grilla.php
@@ -216,8 +216,10 @@
 	<!--Navegador-->
 	<script src="../model/js/ajaxBackupProvincias.js"></script>
 	<script src="../model/js/ajaxMostrarBackupProvincias.js"></script>
+	
 	<script src="../model/js/ajaxBuscarProvincias.js"></script>
 	<script src="../controller/js/navegador.js"></script>
+	
 	<script src="../model/js/ajaxDeshabilitarUsuario.js"></script>
 	<script src="../model/js/ajaxEditarUsuario.js"></script>
 	<script src="../model/js/ajaxEditarPaciente.js"></script>
