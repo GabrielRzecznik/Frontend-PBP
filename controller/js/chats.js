185b9a29f7eb3c6a9190db206ec9ba6951c0d74b
Gabriel Rzecznik <rzecznike@gmail.com>
2022-10-10 22:43:37 -0300

    test

--- controller/js/chats.js
+++ controller/js/chats.js
@@ -1,1 +1,41 @@
 parametro = null;
+
+//Iniciar desde un chat determinado
+let chat = location.hash;
+chat = chat.slice(1);
+
+
+
+//#region Validación de Campos
+const inputMensaje = document.querySelectorAll('#formularioEnviarMensaje input');
+
+const validarFormularioMensaje = (e) => {
+    $mensaje = document.getElementById('mensaje').value;
+    if ($mensaje !== "") {
+        document.getElementById('botonEnviarMensaje').disabled = false;
+    }else{
+        document.getElementById('botonEnviarMensaje').disabled = true;
+    }
+};
+
+inputMensaje.forEach((input) => {
+    input.addEventListener('keyup' , validarFormularioMensaje);//cuando levanto la tecla se ejecuta un codigo
+    input.addEventListener('blur' , validarFormularioMensaje);//cuando me salgo y preciono fuera del input
+});
+//#endregion
+
+//#region Envia Formulario
+const formularioEnviarMensaje = document.getElementById('formularioEnviarMensaje');
+
+formularioEnviarMensaje.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    $mensajeValue = document.getElementById('mensaje').value;
+    
+    if ($mensajeValue !== "") {
+        document.getElementById('mensaje').value = "";
+    }
+}); 
+//#endregion
+
+
