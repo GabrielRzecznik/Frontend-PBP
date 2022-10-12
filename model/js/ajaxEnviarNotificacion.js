ffe21cf6f04738f874755b21c3e4d6a218754926
Gabriel Rzecznik <rzecznike@gmail.com>
2022-10-12 01:49:28 -0300

    test

--- model/js/ajaxEnviarNotificacion.js
+++ model/js/ajaxEnviarNotificacion.js
@@ -2,6 +2,7 @@ function enviarNotificacion(id_evento, tipoNoti){
     //Armar switch
     switch (tipoNoti) {
         case "Solicitud recibida":
+        case "Solicitud canceladas":
         case "Solicitud aceptada":
         case "Solicitud rechazada":
             var formJSON=JSON.stringify({"id_evento":id_evento, "tipoNoti":tipoNoti});
