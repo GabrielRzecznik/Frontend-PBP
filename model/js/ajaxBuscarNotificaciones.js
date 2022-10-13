3f511f5469746c8bd8ae8ddf8f8e5bdc7a945420
Gabriel Rzecznik <rzecznike@gmail.com>
2022-10-12 23:57:03 -0300

    test

--- model/js/ajaxBuscarNotificaciones.js
+++ model/js/ajaxBuscarNotificaciones.js
@@ -1,19 +1,20 @@
 let notificaciones = document.getElementById('notificaciones');
 
 function buscarNotificaciones(){
-    var formData= new FormData();
+    var formData = new FormData();
     formData.append("id_paciente", localStorage.getItem("id_paciente"));
     if (localStorage.getItem("id_profesional") !== "") {
         formData.append("id_profesional", localStorage.getItem("id_profesional"));
     }else{
         formData.append("id_profesional", "0");
     }
-    var formJSON=JSON.stringify(Object.fromEntries(formData));
     
+    var formJSON = JSON.stringify(Object.fromEntries(formData));
+
     xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {//Cuando hay cambio de estado disparo la function
-        if (xmlhttp.readyState == XMLHttpRequest.DONE) {//Volvio respuesta
-            if (xmlhttp.status == 200) {//Volvio Bien
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+            if (xmlhttp.status == 200) {
                 var data=JSON.parse(xmlhttp.responseText);
                 
                 notificaciones.innerHTML = '';
@@ -69,7 +70,7 @@ function buscarNotificaciones(){
             }
         }
     }
-    console.log(formJSON);
+   
     xmlhttp.open("POST",'https://backend-pbp.herokuapp.com/Notificaciones/buscarNotificaciones',true);
     xmlhttp.send(formJSON);
 }
