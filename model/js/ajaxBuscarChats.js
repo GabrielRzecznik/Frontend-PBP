3d95a0c62133c662b9e2df458c23d5d971f2bc87
Gabriel Rzecznik <rzecznike@gmail.com>
2022-10-16 02:41:48 -0300

    test

---
+++ model/js/ajaxBuscarChats.js
@@ -0,0 +1,25 @@
+function buscarChats(){
+    var formData= new FormData();
+    formData.append("id_paciente", localStorage.getItem("id_paciente"));
+    if (localStorage.getItem("id_profesional") != "") {
+        formData.append("id_profesional", localStorage.getItem("id_profesional"));
+    }else{
+        formData.append("id_profesional", 0);
+    }
+    var formJSON=JSON.stringify(Object.fromEntries(formData));
+   
+    console.log(formJSON);
+    
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {//Cuando hay cambio de estado disparo la function
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {//Volvio respuesta
+            if (xmlhttp.status == 200) {//Volvio Bien
+               
+            }else{
+                
+            }
+        }
+    }
+    xmlhttp.open("POST",'https://backend-pbp.herokuapp.com/Chats/buscarChats',true);
+    xmlhttp.send(formJSON);
+}
