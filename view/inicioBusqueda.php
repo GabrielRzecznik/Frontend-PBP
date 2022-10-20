09c815f2216f83dc2e1ace82f58cb8e4289bfe2b
Gabriel Rzecznik <rzecznike@gmail.com>
2022-10-20 14:08:26 -0300

    test

--- view/inicioBusqueda.php
+++ view/inicioBusqueda.php
@@ -137,6 +137,7 @@
 			</div>
 		</div>
 	</div>
+	<br>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 	<script src="../model/js/ajaxBackupProvincias.js"></script>
 	<script src="../model/js/ajaxMostrarBackupProvincias.js"></script>
